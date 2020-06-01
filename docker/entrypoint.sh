db9c355cdd5d1f3888be6bee96a5e2ce3a594e71
Marcel Kallen <root@TC-NB-MK.tecno.local>
2020-06-01 23:36:37 +0200

    removed debug message

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,5 @@ sed -i "s/\"token\": \"\"/\"token\": \"$DISCORD_TOKEN\"/g" config.json
 sed -i "s/\"guild\": \"\"/\"guild\": \"$DISCORD_GUILD\"/g" config.json
 sed -i "s/\"channel\": \"\"/\"channel\": \"$DISCORD_CHANNEL\"/g" config.json
 sed -i "s/\"port\": \"37405\"/\"port\": \"$SERVER_PORT\"/g" config.json
-cat config.json
 
 node .
